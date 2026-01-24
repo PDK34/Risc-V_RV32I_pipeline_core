96afd53a61c1abe8e4678113e8dc6ea6a821b21f
Parthip <projectcs34@gmail.com>
2026-01-24 19:52:06 +0530

    Add immExtend.v

---
+++ src/immExtend.v
@@ -0,0 +1,16 @@
+module immExtend (
+    input [1:0] immSrc,
+    input [31:0] instrD,
+    output reg [31:0] immExtendedD
+);
+    always @(*) begin
+        case (immSrc)
+            2'd0 : immExtendedD = {{20{instrD[31]}} , instrD[31:20]};
+            2'd1 : immExtendedD = {{20{instrD[31]}} , instrD[31:25] , instrD[11:7]};
+            2'd2 : immExtendedD = {{20{instrD[31]}} , instrD[7] , instrD[30:25] , instrD[11:8] , 1'b0};
+            2'd3 : immExtendedD = {{12{instrD[31]}} , instrD[19:12] , instrD[20] , instrD[30:21] , 1'b0};
+            default: immExtendedD = 32'b0;
+        endcase
+    end
+    
+endmodule
