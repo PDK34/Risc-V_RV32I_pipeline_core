bbc64ce9dd923413c0de3fc680f29eede2858baa
Parthip <projectcs34@gmail.com>
2026-01-25 10:55:49 +0530

    Fix  mux3_1.v

--- src/mux3_1.v
+++ src/mux3_1.v
@@ -1,10 +1,10 @@
 module mux3_1 (
     input [31:0] a,b,c,
     input [1:0] sel,
-    output [31:0] y
+    output [31:0] out
 );
-    assign y = (sel == 2'd0) ? a :
+    assign out = (sel == 2'd0) ? a :
                  (sel == 2'd1) ? b :
-                 (sel == 2'd2) ? c ;
-                                 
+                 (sel == 2'd2) ? c :
+                                32'd0;
 endmodule
