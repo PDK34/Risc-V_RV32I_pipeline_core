09c6db7a493e558cc437ff96e247b3f311a698a5
Parthip <projectcs34@gmail.com>
2026-03-20 19:37:24 +0530

    Add stall logic for lw and branch misprediction logic for beq
    Stall logic for lw instruction to flush execute state registers and stall decode and fetch stage registers
    Branch assumed to be not taken .So ,when branch mispredicted ,flush
    Decode and Execute stage pipeline registers

--- src/hazard_unit.v
+++ src/hazard_unit.v
@@ -3,11 +3,16 @@ module hazard_unit (
     input RegWriteM,RegWriteW,
 
     input [1:0] ResultSrcE,
-    input [4:0] RdE,Rs1D,Rs2D
+    input [4:0] RdE,Rs1D,Rs2D,
 
+    input PCSrcE,
 
-    output reg [1:0] forwardAE,forwardBE
+    output reg [1:0] forwardAE,forwardBE,
+    output wire StallF,StallD,FlushE,
+    output wire FlushD
 );
+    reg lwStall;
+
     always@(*)begin
         if((Rs1E == RdM)&RegWriteM&(Rs1E!=5'd0)) forwardAE = 2'b10;
         else if((Rs1E == RdW)&RegWriteW&(Rs1E!=5'd0)) forwardAE = 2'b01;
@@ -20,6 +25,15 @@ module hazard_unit (
     end
 
     always@(*) begin
-        
+        if (ResultSrcE == 2'b01 && ((Rs1D==RdE)|(Rs2D == RdE))) begin
+            lwStall = 1'b1;
+        end
+        else lwStall = 1'b0;
     end
+
+    assign StallF = lwStall;
+    assign StallD = lwStall;
+    assign FlushE = lwStall|PCSrcE;
+    assign FlushD = PCSrcE;
+
 endmodule
